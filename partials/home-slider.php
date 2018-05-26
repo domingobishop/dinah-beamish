f0ba2a4eed8e1cd89b439c7b046484abb2f03435
Christopher Bishop <domingobishop@gmail.com>
2018-05-26 14:19:23 +0100

    slider

--- partials/home-slider.php
+++ partials/home-slider.php
@@ -1,7 +1,43 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: chris
- * Date: 26/05/2018
- * Time: 1:30 PM
- */
+<div class="container">
+    <div class="row">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/slide-1.jpg" alt="‘Cellist Dinah Beamish">
+                    <div class="carousel-caption">
+                        <h3>‘Cellist Dinah Beamish</h3>
+                        <p>‘Cellist Dinah Beamish has played with numerous ensembles and her name appears in the
+                            string sections on albums of many popular artists, such as Jools Holland and Peter Gabriel.</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/slide-2.jpg" alt="Dinah's recent CD New Terrain">
+                    <div class="carousel-caption">
+                        <h3>New Terrain</h3>
+                        <p>"...Soothing, hypnotic, mystical music ...like lying an an ocean of sound..." - Mel Hoffstead.
+                            Dinah's recent CD ‘New Terrain', ambient mellow ‘Cello over silvery Indian tamboura,
+                            is available for purchase.</p>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/slide-3.jpg" alt="Raga Unravelled">
+                    <div class="carousel-caption">
+                        <h3>Raga Unravelled</h3>
+                        <p>Dinah has done some serious study of Raga both in India and the School of Oriental and
+                            African Studies. Transferring Raga onto the ‘cello, she has had lessons with Indian
+                            musicians Rahamit Khan, Baluji Shrivastav, Mehboob Nadim, and has performed with
+                            tabla players Sanju Sahai and Hanif Khan.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
