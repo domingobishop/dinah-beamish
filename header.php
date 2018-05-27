f582f95e072381bd85f3efc3c2995763304794dd
Christopher Bishop <domingobishop@gmail.com>
2018-05-27 15:05:39 +0100

    update viewport

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 <html lang="en-gb">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta content="initial-scale = 1.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>
         <?php wp_title('|', true, 'right'); ?>
     </title>
